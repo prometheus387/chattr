956d3d7c6202573da727ee423f0f4c95b52edae0
prometheus387 <ghgcoder@proton.me>
2026-06-09 19:35:46 +0200

    added navbar component to layout

---
+++ chattr_Frontend/src/components/navigation.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log('Navigation component mounted');
+    });
+</script>
+<div>
+
+</div>
--- chattr_Frontend/src/routes/+page.svelte
+++ chattr_Frontend/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="mx-auto flex flex-col justify-start items-center w-full">
+    <h1>Welcome to Chattr</h1>
+</div>
